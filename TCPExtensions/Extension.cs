b0878719fb5640619120a8db69a702b3799e274b
BOMB-IRON22 <110793614+BOMB-jpg@users.noreply.github.com>
2024-02-18 12:11:08 +0800

    Update Extension.cs

--- TCPExtensions/Extension.cs
+++ TCPExtensions/Extension.cs
@@ -6,6 +6,7 @@ namespace TCPExtensions
     public static class Extension
     {
         public static List<T> Filter<T>(this List<T> records, Func<T, bool> func)
+            //this 关键字表示这是一个扩展方法，它可以在泛型列表类型上直接调用，而不需要通过静态类的实例来调用。
         {
             List<T> filteredList = new List<T>();
 
